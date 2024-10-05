20db6d5cdd6c06fb145499ce86c83577b9efe0fd
Mahdi Sarafraz <hardbitten7697@gmail.com>
2024-10-05 12:53:59 +0330

    fix: Opcodes number redefined.

--- serializers/serializer.player.message.say.go
+++ serializers/serializer.player.message.say.go
@@ -8,11 +8,11 @@ import (
 func SerializePlayerMessageChat(PlayerID uint32, MessageLength uint16, Message string) *utils.ByteBuffer {
 	buffer := utils.NewByteBuffer()
 
-	buffer.WriteUInt16(opcodes.SMSG_MESSAGECHAT)
-	buffer.WriteUInt16(opcodes.SMSG_MESSAGECHAT) // Server opcode for broadcasting the message
-	buffer.WriteUInt32(PlayerID)                 // Player's ID who sent the message
-	buffer.WriteUInt16(MessageLength)            // Include the message length
-	buffer.WriteString(Message)                  // Write the actual message string
+	buffer.WriteUInt16(opcodes.SMSG_OPCODE_MESSAGECHAT)
+	buffer.WriteUInt16(opcodes.SMSG_OPCODE_MESSAGECHAT) // Server opcode for broadcasting the message
+	buffer.WriteUInt32(PlayerID)                        // Player's ID who sent the message
+	buffer.WriteUInt16(MessageLength)                   // Include the message length
+	buffer.WriteString(Message)                         // Write the actual message string
 
 	return buffer
 }
