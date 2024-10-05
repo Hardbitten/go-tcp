499562f274571695c0b860df95578499380aaa4d
Mahdi Sarafraz <hardbitten7697@gmail.com>
2024-10-05 13:54:30 +0330

    fix: MessageChat Packet.

--- serializers/serializer.player.message.say.go
+++ serializers/serializer.player.message.say.go
@@ -7,8 +7,6 @@ import (
 
 func SerializePlayerMessageChat(PlayerID uint32, MessageLength uint16, Message string) *utils.ByteBuffer {
 	buffer := utils.NewByteBuffer()
-
-	buffer.WriteUInt16(opcodes.SMSG_OPCODE_MESSAGE_CHAT)
 	buffer.WriteUInt16(opcodes.SMSG_OPCODE_MESSAGE_CHAT) // Server opcode for broadcasting the message
 	buffer.WriteUInt32(PlayerID)                         // Player's ID who sent the message
 	buffer.WriteUInt16(MessageLength)                    // Include the message length
